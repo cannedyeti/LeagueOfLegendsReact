1b2bcad8ad50e612cd3f0af3ca8893603ea4b285
cannedyeti <cpotebnya@gmail.com>
2017-06-26 20:31:13 -0700

    NEED TO FIX THIS DAMN SMITE ISSUE AHHHH

--- lol/src/components/ultimate-bravery/SelectedChampion.js
+++ lol/src/components/ultimate-bravery/SelectedChampion.js
@@ -1,18 +1,32 @@
 const React = require('react');
 const PropTypes = require('prop-types');
+const ItemSelect = require('./ItemSelect');
+const SumAPI = require('../../utilities/SummonerSpellApi');
 
 class SelectedChampion extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      champ: props.champ
+      champ: props.champ,
+      smite: false,
+      summoners: []
     }
   }
+
+  randomAdjective() {
+    var choices = ['Lit', 'Dank', '1%', 'Dick-sucking', 'Fukkboi', 'Long Schlong', 'Feeder', 'FaZe', 'Clavo\'s Bitch', 'Fuck Trent', 'Gay', 'Thicc']
+    return (choices[Math.floor(Math.random()*choices.length)]);
+  }
+
+
   render() {
     var champion = this.state.champ
     return(
       <div>
         <img alt={champion.name} src={"http://ddragon.leagueoflegends.com/cdn/7.12.1/img/champion/" + champion.image.full} />
+        <h3>{this.randomAdjective()} {champion.name}</h3>
+        {/*<img src={'http://ddragon.leagueoflegends.com/cdn/7.12.1/img/spell/'}/>*/}
+        <ItemSelect smiteBool={this.state.smite}/>
       </div>
     )
   }
